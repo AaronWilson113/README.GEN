211abb18cf6cb0598d1a0b61cdaa994c2f71643d
President <aaron.m.wilson@uconn.edu>
2022-07-20 15:58:02 -0400

    added all the info from the previous repo

---
+++ README.md
@@ -0,0 +1,38 @@
+# Aarons project
+  ## Description
+  Its cool ig
+  ![License Badge](https://shields.io/badge/license-MIT-green)
+  ## Installation 
+  big steps
+  ## Usage
+  by pressing buttons
+  ## Contributing
+  give me money
+  ## tests
+  by giving me money
+  ## Liscense 
+  function renderLicenseLink(license) {
+  
+  let liscenceLink;
+
+  switch(liscenceLink){
+  case "MIT":
+    liscenceLink = "https://opensource.org/licenses/MIT"
+    break;
+  case "GPL":
+    liscenceLink = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+    break;
+  case "APACHE":
+    liscenceLink = "https://www.apache.org/licenses/LICENSE-2.0"
+    break;
+  case "None":
+    liscenceLink = ""
+    }
+  return liscenceLink;
+}
+  ## table of contents
+  undefined
+  ## Questions
+  undefined
+
+
---
+++ index.js
@@ -0,0 +1,82 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils')
+const liscences = ['MIT' , 'GPL' , 'APACHE' , 'None']
+// TODO: Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your project title?',
+            // validates input
+            validate: (value)=>{ if(value){return true} else {return 'please input a project title to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'How would you describe your project?',
+             // validates input
+             validate: (value)=>{ if(value){return true} else {return 'please describe your project to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: 'What steps do you take to install your application?',
+             // validates input
+             validate: (value)=>{ if(value){return true} else {return 'please add installation instructions to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'usageInfo',
+            message: 'How do you use your application?',
+             // validates input
+             validate: (value)=>{ if(value){return true} else {return 'please input usage instructions to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'contributionGuide',
+            message: 'What are the guidelines for contribution?',
+             // validates input
+             validate: (value)=>{ if(value){return true} else {return 'please input contribution guidelines to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'testInstruction',
+            message: 'How can you test the application?',
+             // validates input
+             validate: (value)=>{ if(value){return true} else {return 'please input test guidelines to continue'}}
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What liscence do you want for your project?',
+            choices: liscences
+
+        }
+    ]);
+};
+
+                 
+                    
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generateMarkdown(data))
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    questions()
+    .then((answers) => writeToFile('README.md' , answers))
+    .then(() => console.log('success!'))
+    .catch((err) => console.error(err));
+    
+}
+
+// Function call to initialize app
+init()
+
+
+
